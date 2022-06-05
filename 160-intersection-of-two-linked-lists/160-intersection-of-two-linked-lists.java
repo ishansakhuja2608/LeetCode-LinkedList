f4eead6eee8c1ef18801842d8452c6016d557756
Ishan Sakhuja <84624937+ishansakhuja2608@users.noreply.github.com>
2022-06-05 23:24:16 +0530

    Time: 2 ms (47.08%), Space: 55.7 MB (27.72%) - LeetHub

---
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null)
+            return null;
+        ListNode l1 = headA;
+        ListNode l2 = headB;
+        while(l1 != l2) {
+            l1 = l1 == null ? headB : l1.next;
+            l2 = l2 == null ? headA : l2.next;
+        }
+        return l1;
+    }
+}
+/*
+if l1 and l2 have different lengths ,then the end of l1 will start from the head of l2 and vice versa, due to this, after a specific number of iterations, both the pointers will end up at equal length, and we can check if there's any intersection point
+*/
