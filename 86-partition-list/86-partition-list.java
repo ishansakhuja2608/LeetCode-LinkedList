6b70f34d00756f1c0cd9aa47adfe7958599cdd91
Ishan Sakhuja <84624937+ishansakhuja2608@users.noreply.github.com>
2022-06-13 17:15:41 +0530

    Time: 1 ms (56.15%), Space: 42.5 MB (47.83%) - LeetHub

--- 86-partition-list/86-partition-list.java
+++ 86-partition-list/86-partition-list.java
@@ -10,10 +10,11 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode left = new ListNode(0);
+        // Initialize 2 parts, left and right, left stores less than x and right stored greater than or equal to x.
+        ListNode left = new ListNode(0); 
         ListNode right = new ListNode(0);
-        ListNode referenceLeft = left;
-        ListNode referenceRight = right;
+        ListNode referenceLeft = left; // reference to the head of left partition
+        ListNode referenceRight = right; // reference to the head of right partition
         
         while(head != null) {
             if(head.val < x) {
@@ -27,7 +28,7 @@ class Solution {
             head = head.next;
         }
         right.next = null;
-        left.next = referenceRight.next;
+        left.next = referenceRight.next; // merge both the partitions
         return referenceLeft.next;
     }
 }
